1eb31515c0dc334d2db6d73bf4d6c562a35c08d1
Arthur-ofk <arthauz19@gmail.com>
2024-05-11 15:48:55 +0300

    laB1

---
+++ src/main/java/org/example/jnest/JNestApplication.java
@@ -0,0 +1,13 @@
+package org.example.jnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jnest/JNestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
